48bc772bd2cbb354a6eb39713ce828615d07d56d
abdurrahman ali <abdurr.ali17@gmail.com>
2025-09-21 23:18:11 +0700

    add: add dotfile from sn0wflakes machine

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Sn0wflakes-Dotfiles
-Personal dotfiles for nixos
+# Dotfiles
+
+My personal dotfiles for NixOS.
+
+## Managed With
+- [GNU Stow](https://www.gnu.org/software/stow/) for symlink management  
+- [Home Manager](https://nix-community.github.io/home-manager/) for NixOS integration  
+
+## Structure
+Each directory inside this repo represents a configuration for a specific program
+Dotfiles/
+├── alacritty/
+│ └── alacritty.toml
+├── waybar/
+│ ├── config.jsonc
+│ ├── style.css
+│ └── waybar.sh
+└── hypr/
+└── hyprland.conf
+
+## Usage
+
+Clone this repository:
+
+```bash
+git clone git@github.com:username/Dotfiles.git ~/Dotfiles
+cd ~/Dotfiles 
+```
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  
+  {
+    "pmizio/typescript-tools.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
